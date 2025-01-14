4fec6cbea0c0df07b8b3b91a301302f6fef93955
apvasquezo <nanitave@gmail.com>
2025-01-13 21:56:19 -0500

    configura bD

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
+from sqlalchemy import create_engine
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-<<<<<<< HEAD
-    DATABASE_URL: str = "postgresql://postgres:@localhost:5432/riskmaltern"  # ejemplo
-=======
-    DATABASE_URL: str = "postgresql://postgres:123456789@localhost:5432/riskmanegement"  # ejemplo
->>>>>>> 840fcb4799451c6aa5899b844483fd2d7018cb7e
+    DATABASE_URL: str = "postgresql://postgres:risk@localhost:5432/riskcontrol" # ejemplo
 
     class Config:
         env_file = ".env"
 
 settings = Settings()
+
+# Configuración del motor SQLAlchemy
+engine = create_engine(settings.DATABASE_URL, echo=True)
